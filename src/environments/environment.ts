26ded62da3390f587b3cfda13f4f0f5ec9a9d809
marcuxo <marcourrutiam@gmail.com>
2019-04-24 08:07:23 -0400

    Change key upss

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyBswkGM-lrDzy_Q8se1C4Ui2pyOt2JPr9k",
+    apiKey: "apiKey from FireBase",
     authDomain: "fire-app-bc2af.firebaseapp.com",
     databaseURL: "https://fire-app-bc2af.firebaseio.com",
     projectId: "fire-app-bc2af",
